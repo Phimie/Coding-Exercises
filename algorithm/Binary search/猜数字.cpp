ea1415bbf5f76b427c61e472b982897a3851dafb
Phimie <Phimiee@outlook.com>
2025-07-02 13:53:01 +0800

    Update

---
+++ algorithm/Binary search/猜数字.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+const int pickedNumber = 42;
+
+int guess(int num)
+{
+    if (num > pickedNumber)
+    {
+        return -1;
+    }
+    else if (num < pickedNumber)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+class Solution
+{
+public:
+    int guessNumber(int n)
+    {
+        int i = 1, j = n;
+        while (i <= j)
+        {
+            int mid = i + (j - i) / 2; // i + (j - i) / 2 这个算式避免了i+j的溢出问题
+            if (guess(mid) == -1)
+            {
+                j = mid - 1;
+            }
+            else if (guess(mid) == 1)
+            {
+                i = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    Solution s1;
+    int n = 100;
+    std::cout << s1.guessNumber(n);
+    return 0;
+}
