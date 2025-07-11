598f522d36212d20ef384a97adc2383a3794dcf3
Phimie <Phimiee@outlook.com>
2025-07-11 13:11:13 +0800

    Update

--- algorithm/Leetcode/LC520检测大写字母.cpp
+++ algorithm/Leetcode/LC520检测大写字母.cpp
@@ -5,7 +5,52 @@
 class Solution
 {
 public:
-    bool detectCapitalUse(string word)
+    bool detectCapitalUse(std::string word)
     {
+        if (word.length() == 1)
+        {
+            return true;
+        }
+        if (word[0] >= 97 && word[1] < 97)
+        {
+            return false;
+        }
+        if (word[0] < 97)
+        {
+            if (word[1] < 97)
+            {
+                for (int i = 1; i < word.length(); i++)
+                {
+                    if (word[i] >= 97)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            if (word[1] >= 97)
+            {
+                for (int i = 1; i < word.length(); i++)
+                {
+                    if (word[i] < 97)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        if (word[0] >= 97)
+        {
+            for (int i = 1; i < word.length(); i++)
+            {
+                if (word[i] < 97)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 };
