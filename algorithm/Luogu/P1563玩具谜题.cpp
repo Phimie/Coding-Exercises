dba07270fc6a8d85da843f9598a8e04c3f0e5fbe
Phimie <Phimiee@outlook.com>
2025-07-21 12:09:07 +0800

    Update

--- algorithm/Luogu/P1563玩具谜题.cpp
+++ algorithm/Luogu/P1563玩具谜题.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
 int main()
 {
-    int n, m;
+    int n, m, RoL, step, index = 0;
     std::cin >> n >> m;
     std::vector<std::string> person(n);
-    std::vector<bool> direction(n);
+    std::vector<int> direction(n);
     for (int i = 0; i < n; i++)
     {
         std::cin >> direction[i] >> person[i];
     }
+    for (int i = 0; i < m; i++)
+    {
+        std::cin >> RoL >> step;
+        if (index < 0)
+            index = index % n + n;
+        if (RoL == 1 && direction[index % n] == 0 || RoL == 0 && direction[index % n] == 1)
+            index += step;
+        else
+            index -= step;
+    }
+    if (index < 0)
+        index = index % n + n;
+    std::cout << person[index % n];
 }
