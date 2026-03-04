d292bede3a79048ef416119b40e0436fc9b10f03
Phimie <Phimiee@outlook.com>
2026-03-04 09:34:39 +0800

    Create LC1493.cpp

---
+++ algorithm/Leetcode/LC1493.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int longestSubarray(std::vector<int>& nums) {
+        
+    }
+};
+
+int main (){
+    Solution s1;
+    std::vector<int> nums = {1,1,0,1};
+    std::cout << s1.longestSubarray(nums) << std::endl;
+    return 0;
+}
