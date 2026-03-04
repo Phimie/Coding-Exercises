479f47fb838d9aae7c7ab6394eef9c5378f7e706
Phimie <Phimiee@outlook.com>
2026-03-04 10:15:05 +0800

    Update LC1493.cpp

--- algorithm/Leetcode/LC1493.cpp
+++ algorithm/Leetcode/LC1493.cpp
@@ -4,13 +4,22 @@
 class Solution {
 public:
     int longestSubarray(std::vector<int>& nums) {
-        
+        int max_len = 0,count = 0,left = 0;
+        for (int i = 0; i < (int)nums.size() ; i++){
+            if (nums[i] == 0) count++;
+            while (count > 1) {
+                if (nums[left] == 0) count--;
+                left++;
+            }
+            max_len = std::max(max_len,i - left);
+        }
+        return max_len;
     }
 };
 
 int main (){
     Solution s1;
-    std::vector<int> nums = {1,1,0,1};
+    std::vector<int> nums = {1};
     std::cout << s1.longestSubarray(nums) << std::endl;
     return 0;
 }
