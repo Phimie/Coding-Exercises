271b3f1438e3b9edd0addd1d80d906ceec34df64
Phimie <Phimiee@outlook.com>
2026-03-09 13:25:29 +0800

    Update LC35.cpp

--- algorithm/Leetcode/LC35.cpp
+++ algorithm/Leetcode/LC35.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 class Solution
@@ -7,13 +6,32 @@ class Solution
 public:
     int searchInsert(std::vector<int> &nums, int target)
     {
-        for (int i = 0; i < nums.size(); ++i)
+        return find(nums, target);
+    }
+
+private:
+    int find(std::vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size(), mid = 0;
+        while (left < right)
         {
-            if (nums[i] >= target)
-            {
-                return i;
-            }
+            mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid;
+            else
+                return mid;
         }
-        return nums.size();
+        return left;
     }
 };
+
+int main()
+{
+    Solution s1;
+    std::vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+    std::cout << s1.searchInsert(nums, target) << std::endl;
+    return 0;
+}
