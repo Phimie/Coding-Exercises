bced940a0485691bfa2c4abbdcdb7c2d68f80a82
Phimie <Phimiee@outlook.com>
2026-03-15 13:35:50 +0800

    Update

---
+++ algorithm/Leetcode/LC303.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+class NumArray
+{
+public:
+    std::vector<int> prefix;
+    NumArray(std::vector<int> &nums)
+    {
+        int n = nums.size();
+        prefix.resize(n + 1, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+    int sumRange(int left, int right)
+    {
+        return prefix[right + 1] - prefix[left];
+    }
+};
