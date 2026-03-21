cab0187750ebcd250a8bb4ed3f7322b80574137b
Phimie <Phimiee@outlook.com>
2026-03-21 16:11:57 +0800

    Update Program.cs

--- C#/02LearningFundamentals/Program.cs
+++ C#/02LearningFundamentals/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 namespace LearningBasics
 {
+    //枚举
     enum E_playerType  //默认为int
     {
         Player1,       //默认为0
@@ -14,11 +15,42 @@ namespace LearningBasics
         Normal
     }
 
+    //结构体
+    struct Car{
+        int battery;   //结构体中声明的变量不能直接初始化
+        string color;     //默认private
+        string name;
+
+        public Car(int battery,string color,string name){
+            this.battery = battery;
+            this.color = color;
+            this.name = name;
+        }
+
+        public void info(){   //修改为public
+            Console.WriteLine("电池容量:{0},颜色:{1},型号名字:{2}",battery,color,name);
+        }
+
+    }
+
+
+
+
     class Program
         {
+            //默认参数
+            static void GetMoney(int money = 10){
+                Console.WriteLine("今天得到了{0}块钱",money);
+            }
+
+            //函数重载,注意:函数重载必须放在类级别中
+            static void GetMoney(int money1,int money2){
+                Console.WriteLine("今天得到了两次钱,一次是{0},第二次是{1}",money1,money2);
+            }
+
             static void Main(string[] args)
             {
-                //枚举
+                #region 枚举
                 E_playerType playerType = E_playerType.Player1;  //声明枚举变量
 
                 switch (playerType)   //枚举和switch常搭配使用
@@ -30,8 +62,9 @@ namespace LearningBasics
                     case E_playerType.Player3:
                         break;
                 }
-
-                //数组
+                #endregion
+                
+                #region 数组
                 int[] arr1 = new int[8] {1,2,3,4,5,6,7,8}; //确定数组大小
                 int[] arr2 = new int[] {1,2,3,4,5,5}; //根据初始化内容确定数组大小
                 int[] arr3 = {1,2,3,4,5,6,7}; //同上
@@ -44,10 +77,61 @@ namespace LearningBasics
                 Console.WriteLine(arr4.GetLength(0));  //行  //通过.GetLength()方法来得到行和列的长度
                 Console.WriteLine(arr4.GetLength(1));  //列
 
+                int[] a = new int[]{10};
+                int[] b = a;
+                b[0]= 20;
+                Console.WriteLine(a[0]); //数组是引用类型,变量存储的是数据在内存中的地址;
+
+                string str1 = "123";
+                string str2 = str1;
+                str2 = "321";
+                Console.WriteLine(str1); //string是特殊的引用类型,它具备值类型的特征
+                Console.WriteLine(str2);
+                #endregion
 
 
+                #region 函数
+                //函数基础语法
+                static void SayHello(string name)
+                {
+                    Console.WriteLine("Hello,{0}",name);
+                    //return; 返回类型是void可以省略return;
+                }
+                SayHello(str1);
+                
+                //ref和out
+                int value = 5;
+                static void ChangeValue_ref(ref int value){  //通过ref传入引用值
+                    value = 3;
+                }
+                static void ChangeValue_out(out int value){
+                    value = 3;
+                }
+                ChangeValue_ref(ref value);   //ref和out的区别:
+                ChangeValue_out(out value);   //1.ref传入的变量必须初始化 out不用
+                Console.WriteLine(value);     //2.out传入的变量必须在内部复制 ref不用
+
+                //变长参数
+                static void PrintAll(params string[] items){ //变长参数关键字params
+                    foreach (string item in items){          //枚举型for循环
+                        Console.WriteLine(item);
+                    }
+                }
+                PrintAll("苹果","香蕉","栗子");
+
+                GetMoney();
+                GetMoney(100);
+                GetMoney(2,3);
+                
+                #endregion
 
 
+                #region 结构体
+                Car car1 = new Car(12,"Red","婴儿车");
+                car1.info();
+
+
+                #endregion
 
                 Console.WriteLine("按任意键继续");
                 Console.ReadKey(true);
