c6bb132a9772b1f00f6f96e887d28c25fba3e477
Phimie <Phimiee@outlook.com>
2026-03-26 19:20:12 +0800

    Update

---
+++ C#/10LearningList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LearningList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // List<T> 是一个泛型集合类，可以存储任意类型的数据
+            // 对比ArrayList，List<T> 是类型安全的，性能更好
+            // 避免了装箱和拆箱的开销
+            // 现代C# 开发中，推荐使用 List<T> 而不是 ArrayList
+            List<int> list = new List<int>();
+            List<string> list2 = new List<string>();
+            List<bool> list3 = new List<bool>();
+
+            //增
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list2.Add("hello");
+            list2.Add("world");
+            list.AddRange(new int[] { 4, 5, 6 });
+            //删
+            list.Remove(2);
+            list.RemoveAt(0);
+
+            //改
+            list[0] = 10;
+
+            //查
+            int index = list.IndexOf(3);
+            int count = list.Count;
+
+            //遍历
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("请按任意键继续...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/11LearningDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+namespace LearningDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<TKey, TValue> 是一个泛型集合类，用于存储键值对
+            //对比HashTable，Dictionary<TKey, TValue> 是类型安全的，并且性能更好
+            //因为避免了装箱和拆箱操作
+            //现代的C#开发中，推荐使用Dictionary<TKey, TValue> 而不是HashTable
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+
+            //增
+            myDictionary.Add("Hello", "A greeting");
+            myDictionary.Add("World", "The earth, together with all of its countries and peoples");
+
+            //删
+            myDictionary.Remove("Hello");
+
+            //改
+            myDictionary["World"] = "The planet we live on";
+
+            //查
+            Console.WriteLine(myDictionary["World"]);
+
+            if (myDictionary.ContainsKey("Hello"))  //检查键是否存在
+            {
+                Console.WriteLine(myDictionary["Hello"]);
+            }
+            else
+            {
+                Console.WriteLine("Key 'Hello' does not exist.");
+            }
+
+
+
+
+
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
